5f9192d416b4fe8f75d7c65bd1a523f60f921e37
mehmetbuyukgumus <mehmetbgumus@gmail.com>
2020-04-05 03:46:38 +0300

    Bug düzeltildi

--- ATM Programı.py
+++ ATM Programı.py
@@ -8,15 +8,15 @@ Yapabileceğiniz İşlemler:
      1.         Para Çekme
      2.         Para Yatırma
      3.         Bakiye Sorgulama
-     4.         Çıkış
+     q.         Çıkış
 -------------------
 """)
 
-bakiye=int(1000)
+bakiye=1000
 
 while True:
-    işlem= int(input("Yapmak İstediğiniz İşlemin Kodunu Giriniz:"))
-    if  işlem==1:
+    işlem= input("Yapmak İstediğiniz İşlemin Kodunu Giriniz:")
+    if  işlem=="1":
         para_çekme=int(input("Çekmek İstediğiniz Tutarı Giriniz:"))
         if bakiye-para_çekme>=0:
             bakiye-=para_çekme
@@ -24,15 +24,15 @@ while True:
         elif    bakiye-para_çekme<=0:
             print("Lütfen Başka Bir İşlem Seçiniz")
             continue
-    elif  işlem==2:
+    elif  işlem=="2":
         para_yatırma=int(input("Yatımak İstediğiniz Tutarı Yazınız:"))
         bakiye+=para_yatırma
         print("Bakiyeniz {} TL'dir".format(bakiye))
         continue
-    elif işlem==3:
+    elif işlem=="3":
         print(bakiye)
         continue
-    elif işlem==str("q":)
+    elif işlem=="q":
         print("Tekrar Bekleriz")
         break
     else:
